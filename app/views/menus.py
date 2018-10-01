62fa5fff353c4d781d2b68b343af97c29f437af8
Ruiru11 <erastusruiru11gmail.com>
2018-10-01 11:15:16 +0300

    add menu class and methods

--- app/views/menus.py
+++ app/views/menus.py
@@ -1,15 +1,17 @@
 from flask import Blueprint
 from flask_restful import reqparse
 from app.controllers.menus import Menus
+from app.controllers.users import Users
 
 mod_menus = Blueprint('menus', __name__, url_prefix='/api/v2')
 
 mnu = Menus()
+usr = Users()
 
 @mod_menus.route('/menus', methods=['POST'])
-def create_menu():
+@usr.logged_in
+def create_menu(res=None,user_id=None):
     parser = reqparse.RequestParser()
-    parser.add_argument('meal_id', type=int, location='json')
     parser.add_argument('name', type=str, location='json')
     parser.add_argument('description', type=str, location='json')
     data = parser.parse_args()
@@ -17,10 +19,12 @@ def create_menu():
     return mnu.create_menu(data)
 
 @mod_menus.route('/menus', methods=['GET'])
-def get_menu():
+@usr.logged_in
+def get_menu(res=None,user_id=None):
     return mnu.get_menu()
 
 
 @mod_menus.route('/menus/<int:id>', methods=['GET'])
+@usr.logged_in
 def get_menus(id):
     return mnu.get_menus(id)
