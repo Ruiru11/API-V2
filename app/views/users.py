b92d821d340df8299293be6a0bdd28a05978c423
Ruiru11 <erastusruiru11gmail.com>
2018-10-01 11:15:40 +0300

    add regestration and login

--- app/views/users.py
+++ app/views/users.py
@@ -7,17 +7,25 @@ mod_users = Blueprint('users', __name__, url_prefix='/api/v2')
 usr = Users()
 
 @mod_users.route('/signup', methods=['POST'])
-def create_user():
+def signup():
     parser = reqparse.RequestParser()
-    parser.add_argument('id',type=int, location="json")
-    parser.add_argument('email',type=str, location="json")
-    parser.add_argument('address',type=str,location="json")
-    parser.add_argument('password',type=str,location="json")
-    parser.add_argument('username',type=str,location="json")
+    parser.add_argument('email',type=str, required=True, help="Pease provide an email", location="json")
+    parser.add_argument('address',type=str, required=True, help="Pease provide an address", location="json")
+    parser.add_argument('password',type=str,required=True, help="Pease provide a password",location="json")
+    parser.add_argument('username',type=str, required=True, help="Pease provide username",location="json")
     data = parser.parse_args()
-
     return usr.create_user(data)
 
+@mod_users.route('/signin', methods=['POST'])
+def signin():
+    parser = reqparse.RequestParser()
+    parser.add_argument('email',type=str, required=True, help="Pease provide an email", location="json")
+    parser.add_argument('password',type=str,required=True, help="Pease provide a password",location="json")
+    data = parser.parse_args()
+    return usr.sign_in(data)
+
 @mod_users.route('/users', methods=['GET'])
-def get_users():
-    return usr.get_users()
+@usr.logged_in
+def get_users(user_id=None, res=None):
+    print(usr.all_users())
+    return usr.all_users()
