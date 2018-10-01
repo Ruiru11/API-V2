cfa92b4fe6410c5badb733cea193664992cac216
Ruiru11 <erastusruiru11gmail.com>
2018-10-01 10:41:04 +0300

    modified

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from .config import config_by_name
+
+bcrypt = Bcrypt()
+
 from app.models.Database import Database_connection
 from app.views.orders import mod_orders
 from app.views.users import mod_users
@@ -7,10 +11,10 @@ from app.views.menus import mod_menus
 
 db=Database_connection()
 
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
-    db
     app.register_blueprint(mod_orders)
     app.register_blueprint(mod_users)
     app.register_blueprint(mod_menus)
